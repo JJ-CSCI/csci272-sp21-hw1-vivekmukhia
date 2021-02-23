56cf9596763769b8a8f3eab6809ecbb0fa078a8e
vivekmukhia <vivek.mukhia@jjay.cuny.edu>
2021-02-23 03:09:57 +0000

    new code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,36 @@
 #include "catch.hpp"
 #include "binomial.h"
 //------------------------------
+#ifndef Binomial_h
+#define Binomial_h
+class Binomial {
+private:
+  float coef[3] {0, 1.0, 1.0};
+  int pow [3] {0, 1, 1};
+public:
+  Binomial();
 
+  Binomial(float);
+  
+  Binomial(float x, int y);
+
+  Binomial(float x, int y, float a);
+  
+  Binomial(float x, int a, float y, int b);
+  
+  int GetPower(int i);
+
+  float GetCoefficient(int m);
+
+  int SetPower(int e, int f);
+
+  int Add(Binomial x);
+
+  void Multiply(float x);
+
+  void Multiply(float x, int y);
+};
+#endif
 //------------------------------
 //   DO NOT MODIFY TEST CASES
 //------------------------------
